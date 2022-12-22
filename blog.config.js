3df5f2d8eff9bacd34010798e45a523a2cf184d2
Anis AYARI <anis.ayari.perso@gmail.com>
2022-12-22 14:38:17 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -37,7 +37,7 @@ const BLOG = {
   telegramChatId: '', // The chat id of your Telegram bot
   telegramChannelUrl: '', // The link of your Telegram channel
   telegramChannelName: '', // The name of your Telegram channel
-  craftConfigShareUrl: '', // The link to share your craft config
+  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee and Umami, please fill with 'ga' or 'ackee' or 'umami', leave it empty to disable it.
     umamiConfig: {
